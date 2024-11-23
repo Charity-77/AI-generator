8c0d10299de3effdc3e7e7a6567d955257cc532c
Charity-77 <charitykamogelo07@gmail.com>
2024-11-24 01:38:32 +0200

    edited promt and content

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function generatePoem(event){
     let instructionInput = document.querySelector("#user-instructions");
     let apiKey = "04aofb30d291adc1aete1a1f7ab3a4a4";
   let context =
-  "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate french poem about ${instructionInput.value}`;
+  "You are a Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a poem  in English about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
   let poemElement = document.querySelector("#poem");
