3f4534654eb8f198143e546d1731e1655249eff5
Charity-77 <charitykamogelo07@gmail.com>
2024-11-24 00:42:32 +0200

    added styling and more javascript  operation

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function generatePoem(event){
   let context ="You are a romantic poem expert and love to write short poem .Your  mission is to generate a 4 line poem  and seperate eachline with a <br/> .Make sure to follow user instructions .Do not include tittle to the poem sign the poem with 'shecoodes AI' inside a <strong> element ";
   let prompt = `User instructions: Generate french poem about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    let poemElement=document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
   
   axios.get(apiUrl).then(displayPoem);
 
